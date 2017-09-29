177d2d1f9842cf42d0f279e8fdb0170baf805465
GigByt3 <p3n.duke@gmail.com>
2017-09-29 06:28:06 -0700

    Create Terrain_Gennerator.js

---
+++ Terrain_Gennerator.js
@@ -0,0 +1,12 @@
+var map = [[0,"mountains"]]
+
+function fill(map) {
+	if(highest_variable = 1; highest variable > 100^100; highestvariable++) {
+		var terrain = random_terrain(highestvariable, map)
+		map.push([highest_variable, terrain])
+	}
+}
+
+function random_terrain(number, array) {
+	switch()
+}
