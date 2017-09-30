ec41ca934449a54ca81778e690ab822274bab118
GigByt3 <p3n.duke@gmail.com>
2017-09-29 18:20:28 -0700

    Update Terrain_Gennerator.js

--- Terrain_Gennerator.js
+++ Terrain_Gennerator.js
@@ -1,5 +1,5 @@
-var map = [[0,"mountains"]]
-
+var map = [[0,0]]
+//0:mountains 1:forest 2: plains 3: occean
 function fill(map) {
 	if(highest_variable = 1; highest variable > 100^100; highestvariable++) {
 		var terrain = random_terrain(highestvariable, map)
@@ -8,16 +8,41 @@ function fill(map) {
 }
 
 function random_terrain(number, array) {
-	if(number/1000000 % 2 == 0) {
-	
-	} else if(number/1000000 % 2 == 1) {
-		
-	} else if() {
-			  
-	} else if() {
-		
+	if(number/1000000 % 2 == 1) {
+		//top
+		var mountains = 0.25
+		var forest = 0.25
+		var plains = 0.25
+		var occean = 0.25
 	} else {
-		
+		var mountains = 0.15
+		var forest = 0.15
+		var plains = 0.15
+		var occean = 0.15
+		for(i = 0; i > 1; i++) {
+			switch(array[number-1000000 + i][1]) {
+				case 0;
+				mountains +=0.20
+				break;
+				case 1;
+				forest +=0.20
+				break;
+				case 2;
+				plains +=0.20
+				break;
+				case 3;
+				occean +=0.20
+			}
+		}
 	}
-			  
+	var randomNum = Math.random
+	if(randomNum < mountains){
+		return array[number].push(0)				
+	} else if(randomNum < mountains + forest){
+		return array[number].push(1)				
+	} else if(randomNum < mountains + forest + plains){
+		return array[number].push(2)				
+	} else if(randomNum < mountains + forest + plains + occean){
+		return array[number].push(3)				
+	} else
 }
